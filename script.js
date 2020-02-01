f6d0a0d053a862ad41ecb350d95619ac7dabfc97
millernj <noahjmiller17@gmail.com>
2020-02-01 15:44:16 -0800

    dynamically rendering hours

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let currentDate = moment();
 
 const incrementCurrentDate = () => {
   currentDate.add(1, 'days');
+  renderHours();
 }
 
 const decrementCurrentDate = () => {
   currentDate.subtract(1, 'days');
+  renderHours();
 }
 
 const renderCurrentDate = () => {
@@ -27,8 +29,64 @@ const updateClock = () => {
   $('#second').css("transform", "rotate(" + second + "deg)");
 }
 
-$(document).ready(
+const renderHours = () => {
+  $('#scheduler').empty();
+  for (let i = 9; i < 18; i++) {
+    $('#scheduler').append(buildHourContainer(currentDate.hour(i)))
+  }
+}
+
+const buildHourContainer = (datetime) => {
+
+  const hourString = datetime.format('h a');
+  const hourId = datetime.format('MM_DD_Y_HH');
+
+  const activity = localStorage.getItem(hourId);
+  const hourContainer = $('<li>');
+  hourContainer.addClass('row hour-container');
+
+  const hourLabel = $('<h5>');
+  hourLabel.text(hourString);
+  hourLabel.addClass('col-2 hour-label');
+  
+  const activityForm = $(`<form id="${hourString}-form"></form>`);
+  const activityInput = $(`<input type="text" data-id="${hourId}">`);
+  
+  const activityButton = $(`<button type="submit" form="${hourString}-form"></button>`);
+  const saveIcon = $('<i>');
+  saveIcon.addClass('far fa-save');
+  saveIcon.attr('aria-hidden', true);
+
+  if (activity) {
+    activityInput.val(activity);
+  }
+
+  activityForm.addClass('col-10');
+  activityForm.submit((event) => {
+    event.preventDefault();
+    const activity = activityInput.val().trim();
+    localStorage.setItem(hourId, activity);
+  });
+
+  activityButton.addClass('btn btn-dark');
+  activityButton.css({'outline': 'none', 'border': 'none'});
+  activityButton.append(saveIcon);
+
+  activityForm.append(activityInput, activityButton);
+  hourContainer.append(hourLabel, activityForm);
+  
+  if (datetime > currentDate) {
+
+  }
+  hourContainer
+  return hourContainer;
+}
+
+
+
+$(document).ready(() => {
   setInterval(
     renderCurrentDate, 10
   )
-)
+  renderHours();
+});
